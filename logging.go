884a7aa654c03d29ebe136549467abcac7919c5a
Byte Cats <devin@bytecats.codes>
2023-04-03 22:00:47 -0400

    logging recieved shit

--- logging.go
+++ logging.go
@@ -5,11 +5,31 @@ import (
 	"net/http"
 )
 
+import (
+	"bytes"
+	"io/ioutil"
+)
+
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Log the request information
 		color.White("Request method: %s", r.Method)
 		color.White("Request URL: %s", r.URL.String())
+
+		// Read and log the request body
+		if r.Body != nil {
+			bodyBytes, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, "Error reading request body", http.StatusInternalServerError)
+				return
+			}
+
+			// Log the request body
+			color.White("Request body: %s", string(bodyBytes))
+
+			// Replace the request body with a new reader, so it can be read again by the handlers
+			r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+		}
 
 		// Record the status code using a custom response writer
 		recorder := &statusRecorder{ResponseWriter: w, statusCode: http.StatusOK}
