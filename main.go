1eaad3f1fe6a5fd04eda45ebd9643987e6e3748b
Byte Cats <devin@bytecats.codes>
2023-04-03 22:15:09 -0400

    add main api description

--- main.go
+++ main.go
@@ -9,6 +9,18 @@ import (
 	_ "profiles/docs" // Replace this with the path to your generated docs
 )
 
+// @title User Profile API
+// @version 1.0
+// @description This is a simple API for managing user profiles.
+// @termsOfService http://anij.bytecats.codes/tos/
+// @contact.name API Support
+// @contact.url http://www.anij.bytecats.codes/support
+// @contact.email support@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 func main() {
 	color.Blue("Starting server on port 8080...")
 
